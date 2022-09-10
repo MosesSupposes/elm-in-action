acbac64f9c05c42e8c5e589906dffd1971e58420
MosesSupposes <mosesasamuel633@gmail.com>
2022-09-10 00:18:35 -0500

    refactor our single unit test into a fuzz test

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -2,16 +2,20 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Json.Decode exposing (decodeString)
+import Json.Decode as Decode exposing (decodeValue)
+import Json.Encode as Encode
 import PhotoGroove
 import Test exposing (..)
 
 
 decoderTest : Test
 decoderTest =
-    test "title defaults to (unititled)" <|
-        \_ ->
-            """{"url": "fruits.com", "size": 5}"""
-                |> decodeString PhotoGroove.photoDecoder
+    fuzz2 string int "title defaults to (unititled)" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
