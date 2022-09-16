1efb3139b11cd4c18db9d1ca1939a9a908cc812a
MosesSupposes <mosesasamuel633@gmail.com>
2022-09-15 23:13:07 -0500

    complete the book and therefore PhotoGroove

---
+++ src/PhotoGallery.elm
@@ -0,0 +1,353 @@
+port module PhotoGallery exposing (Model, Msg(..), Photo, Status(..), init, initialModel, main, photoDecoder, subscriptions, update, urlPrefix, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (on, onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder, at, int, string, succeed)
+import Json.Decode.Pipeline as PipelineDecode exposing (optional)
+import Json.Encode as Encode
+import Random
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ] <|
+        case model.status of
+            Loaded photos selectedUrl ->
+                viewLoaded photos selectedUrl model
+
+            Loading ->
+                []
+
+            Errored errorMessage ->
+                [ text ("Error: " ++ errorMessage) ]
+
+
+viewLoaded : List Photo -> String -> Model -> List (Html Msg)
+viewLoaded photos selectedUrl model =
+    [ button [ onClick ClickedSurpriseMe ] [ text "Surprise Me!" ]
+    , div [ class "activity" ] [ text model.activity ]
+    , div [ class "filters" ]
+        [ viewFilter SlidHue "Hue" model.hue
+        , viewFilter SlidRipple "Ripple" model.ripple
+        , viewFilter SlidNoise "Noise" model.noise
+        ]
+    , h3 [] [ text "Thumbnail Size:" ]
+    , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+    , div [ id "thumbnails", class (sizeToClass model.chosenSize) ]
+        (List.map (viewThumbnail selectedUrl) photos)
+    , canvas [ id "main-canvas", class "large" ] []
+    ]
+
+
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick (ClickedPhoto thumb.url)
+        ]
+        []
+
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        -- TODO: Use another event listener other than onClick to listen for radio state changes
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
+        , text (sizeToString size)
+        ]
+
+
+viewFilter : (Int -> Msg) -> String -> Int -> Html Msg
+viewFilter toMsg name magnitude =
+    div [ class "filter-slider" ]
+        [ label [] [ text name ]
+        , rangeSlider
+            [ Attr.max "11"
+            , Attr.property "val" (Encode.int magnitude)
+            , onSlide toMsg
+            ]
+            []
+        , label [] [ text (String.fromInt magnitude) ]
+        ]
+
+
+rangeSlider : List (Attribute msg) -> List (Html msg) -> Html msg
+rangeSlider attributes children =
+    node "range-slider" attributes children
+
+
+
+-- The size as represnted to the user
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "medium"
+
+        Large ->
+            "large"
+
+
+
+-- The size as represnted in our stylesheets
+
+
+sizeToClass : ThumbnailSize -> String
+sizeToClass size =
+    case size of
+        Small ->
+            sizeToString size
+
+        Medium ->
+            "med"
+
+        Large ->
+            sizeToString size
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+
+-- MODEL
+
+
+type Status
+    = Loading
+    | Loaded (List Photo) String
+    | Errored String
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+port setFilters : FilterOptions -> Cmd msg
+
+
+
+-- TODO: Accept a (Json.Decode.Value -> msg) as an input instead of a (String -> msg)
+
+
+port activityChanges : (String -> msg) -> Sub msg
+
+
+type alias FilterOptions =
+    { url : String
+    , filters : List { name : String, amount : Float }
+    }
+
+
+type alias Photo =
+    { url : String
+    , size : Int
+    , title : String
+    }
+
+
+photoDecoder : Decoder Photo
+photoDecoder =
+    succeed Photo
+        -- `succeed` is similar to `return` in Haskell... It lifts the type into the Decoder monad
+        |> PipelineDecode.required "url" string
+        |> PipelineDecode.required "size" int
+        |> optional "title" string "(untitled)"
+
+
+type alias Model =
+    { status : Status
+    , activity : String
+    , chosenSize : ThumbnailSize
+    , hue : Int
+    , ripple : Int
+    , noise : Int
+    }
+
+
+initialModel : Model
+initialModel =
+    { status = Loading
+    , activity = ""
+    , chosenSize = Medium
+    , hue = 5
+    , ripple = 5
+    , noise = 5
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+    | GotRandomPhoto Photo
+    | GotActivity String
+    | GotPhotos (Result Http.Error (List Photo))
+    | SlidHue Int
+    | SlidRipple Int
+    | SlidNoise Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedSize size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
+        ClickedSurpriseMe ->
+            case model.status of
+                Loaded (firstPhoto :: otherPhotos) _ ->
+                    Random.uniform firstPhoto otherPhotos
+                        |> Random.generate GotRandomPhoto
+                        |> Tuple.pair model
+
+                Loaded [] _ ->
+                    ( model, Cmd.none )
+
+                Loading ->
+                    ( model, Cmd.none )
+
+                Errored _ ->
+                    ( model, Cmd.none )
+
+        ClickedPhoto url ->
+            applyFilters { model | status = selectUrl url model.status }
+
+        GotRandomPhoto photo ->
+            applyFilters { model | status = selectUrl photo.url model.status }
+
+        GotActivity activity ->
+            ( { model | activity = activity }, Cmd.none )
+
+        GotPhotos (Ok photos) ->
+            case photos of
+                first :: rest ->
+                    applyFilters { model | status = Loaded photos first.url }
+
+                [] ->
+                    ( { model | status = Errored "0 photos found" }, Cmd.none )
+
+        GotPhotos (Err _) ->
+            ( { model | status = Errored "Server error!" }, Cmd.none )
+
+        SlidHue hue ->
+            applyFilters { model | hue = hue }
+
+        SlidRipple ripple ->
+            applyFilters { model | ripple = ripple }
+
+        SlidNoise noise ->
+            applyFilters { model | noise = noise }
+
+
+applyFilters : Model -> ( Model, Cmd Msg )
+applyFilters model =
+    case model.status of
+        Loaded photos selectedUrl ->
+            let
+                filters =
+                    [ { name = "Hue", amount = toFloat model.hue / 11 }
+                    , { name = "Ripple", amount = toFloat model.ripple / 11 }
+                    , { name = "Noise", amount = toFloat model.noise / 11 }
+                    ]
+
+                url =
+                    urlPrefix ++ "large/" ++ selectedUrl
+            in
+            ( model, setFilters { url = url, filters = filters } )
+
+        Loading ->
+            ( model, Cmd.none )
+
+        Errored _ ->
+            ( model, Cmd.none )
+
+
+selectUrl : String -> Status -> Status
+selectUrl url status =
+    case status of
+        Loaded photos _ ->
+            Loaded photos url
+
+        Loading ->
+            status
+
+        Errored _ ->
+            status
+
+
+onSlide : (Int -> msg) -> Attribute msg
+onSlide toMsg =
+    at [ "detail", "userSlidTo" ] int
+        |> Decode.map toMsg
+        |> on "slide"
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    activityChanges GotActivity
+
+
+
+-- MAIN
+
+
+initialCmd : Cmd Msg
+initialCmd =
+    Http.get
+        { url = "http://elm-in-action.com/photos/list.json"
+        , expect = Http.expectJson GotPhotos (Decode.list photoDecoder)
+        }
+
+
+
+{- TODO: Accept a Json.Decode.Value as a flag instead of a Float, that way we can properly
+   sanitize our JS input before propogating it through our program.
+
+   In the event that an invalid value comes through (i.e. not a Float), the Elm Runtime will
+   catch it before it gets a chance to propogate; however, we should have explicit control over
+   how this error is handled.
+-}
+
+
+init : Float -> ( Model, Cmd Msg )
+init flags =
+    let
+        activity =
+            "Initializing Pasta v" ++ String.fromFloat flags
+    in
+    ( { initialModel | activity = activity }, initialCmd )
+
+
+main : Program Float Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
---
+++ tests/PhotoGalleryTests.elm
@@ -0,0 +1,131 @@
+module PhotoGalleryTests exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Html.Attributes as Attr exposing (src)
+import Json.Decode as Decode exposing (decodeValue)
+import Json.Encode as Encode
+import PhotoGallery exposing (Model, Msg(..), Photo, Status(..), initialModel, photoDecoder, update, urlPrefix, view)
+import Test exposing (..)
+import Test.Html.Event as Event
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (attribute, tag, text)
+
+
+decoderTest : Test
+decoderTest =
+    fuzz2 string int "title defaults to (unititled)" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue photoDecoder
+                |> Result.map .title
+                |> Expect.equal (Ok "(untitled)")
+
+
+sliders : Test
+sliders =
+    describe "slider sets the desired field in the Model"
+        [ testSlider "SlidHue" SlidHue .hue
+        , testSlider "SlidRipple" SlidRipple .ripple
+        , testSlider "SlidNoise" SlidNoise .noise
+        ]
+
+
+testSlider : String -> (Int -> Msg) -> (Model -> Int) -> Test
+testSlider description toMsg amountFromModel =
+    fuzz int description <|
+        \amount ->
+            initialModel
+                |> update (toMsg amount)
+                |> Tuple.first
+                |> amountFromModel
+                |> Expect.equal amount
+
+
+noPhotosNoThumbnails : Test
+noPhotosNoThumbnails =
+    test "no thumbnails render when there are no photos to rendner" <|
+        \_ ->
+            initialModel
+                |> PhotoGallery.view
+                |> Query.fromHtml
+                |> Query.findAll [ tag "img" ]
+                |> Query.count (Expect.equal 0)
+
+
+thumbnailRendered : String -> Query.Single msg -> Expectation
+thumbnailRendered url query =
+    query
+        |> Query.findAll [ tag "img", attribute (Attr.src (urlPrefix ++ url)) ]
+        |> Query.count (Expect.atLeast 1)
+
+
+photoFromUrl : String -> Photo
+photoFromUrl url =
+    { url = url, size = 0, title = "" }
+
+
+thumbnailsWork : Test
+thumbnailsWork =
+    fuzz urlFuzzer "URLS render as thumnails" <|
+        \urls ->
+            let
+                thumbnailChecks : List (Query.Single msg -> Expectation)
+                thumbnailChecks =
+                    List.map thumbnailRendered urls
+            in
+            { initialModel | status = Loaded (List.map photoFromUrl urls) "" }
+                |> view
+                |> Query.fromHtml
+                |> Expect.all thumbnailChecks
+
+
+urlFuzzer : Fuzzer (List String)
+urlFuzzer =
+    Fuzz.intRange 1 5
+        |> Fuzz.map urlsFromCount
+
+
+urlsFromCount : Int -> List String
+urlsFromCount urlCount =
+    List.range 1 urlCount
+        |> List.map (\num -> String.fromInt num ++ ".png")
+
+
+clickThumbnail : Test
+clickThumbnail =
+    describe "selecting thumbnails"
+        [ fuzz3 urlFuzzer string urlFuzzer "clicking a thumbnail selects it" <|
+            \urlsBefore urlToSelect urlsAfter ->
+                let
+                    url =
+                        urlToSelect ++ ".jpeg"
+
+                    photos =
+                        (urlsBefore ++ url :: urlsAfter)
+                            |> List.map photoFromUrl
+
+                    srcToClick =
+                        urlPrefix ++ url
+                in
+                { initialModel | status = Loaded photos "" }
+                    |> view
+                    |> Query.fromHtml
+                    |> Query.find [ tag "img", attribute (Attr.src srcToClick) ]
+                    |> Event.simulate Event.click
+                    |> Event.expect (ClickedPhoto url)
+        , test "selected photos are recognized as such in state" <|
+            \_ ->
+                let
+                    loadedPhotos =
+                        List.map photoFromUrl [ "1.jpeg", "2.jpeg" ]
+                in
+                { initialModel | status = Loaded loadedPhotos "1.jpeg" }
+                    |> update (ClickedPhoto "2.jpeg")
+                    |> Tuple.first
+                    |> .status
+                    |> Expect.equal (Loaded loadedPhotos "2.jpeg")
+        ]
